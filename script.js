6dacddfa84c7d82320f3ef3d350523b4f31468ee
Dan Sajjad <dan.sajjad@gmail.com>
2016-11-23 16:10:17 -0800

    Add event listener for add button. Cache DOM nodes

--- script.js
+++ script.js
@@ -1,3 +1,25 @@
 (function() {
   console.log('hello')
+
+  var notes = [];
+
+  var textArea = document.getElementById('text-area');
+  var addButton = document.getElementById('add-note');
+  var deleteButton = document.getElementById('remove-note');
+
+
+
+  addButton.addEventListener('click', function() {
+    var note = textArea.value;
+    console.log(note);
+    notes.push(note);
+
+
+
+  });
+
+  function appendNote(note) {
+  }
+
+
 })()
