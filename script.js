4bfa0385a4329ba2e0f2732244dfb04fde0a4133
Dan Sajjad <dan.sajjad@gmail.com>
2016-11-23 16:50:45 -0800

    Added clickhandler for add-note

--- script.js
+++ script.js
@@ -2,24 +2,39 @@
   console.log('hello')
 
   var notes = [];
+  var currentNoteIndex = 0;
 
   var textArea = document.getElementById('text-area');
   var addButton = document.getElementById('add-note');
-  var deleteButton = document.getElementById('remove-note');
-
+  var clearButton = document.getElementById('clear-note');
+  var notesContainer = document.getElementById('notes-container');
 
 
   addButton.addEventListener('click', function() {
     var note = textArea.value;
-    console.log(note);
-    notes.push(note);
-
-
-
+    if(note) {
+      notes.push(note);
+      appendNote(note);
+    }
   });
+  
 
   function appendNote(note) {
+    var noteDiv = document.createElement('div');
+    noteDiv.setAttribute("class", "note");
+    var notePara = document.createElement('p');
+
+    notePara.innerText = note;
+    noteDiv.appendChild(notePara);
+    notePara.id = currentNoteIndex;
+    currentNoteIndex++;
+
+    notesContainer.appendChild(noteDiv);
+    console.log(notes)
+    textArea.value = "";
   }
 
 
+
 })()
+
